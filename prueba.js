741c5c14c64648faa42e6a8526cb75ed69768206
Luiis123 <jorgeluis10062005@gmail.com>
2024-10-26 17:01:48 -0700

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,10 +1,33 @@
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
 
+
+
 //Declaramos variables
+let slideIndex = 0;
+const slides = document.querySelectorAll('.carousel-images img');
+const dots = document.querySelectorAll('.carousel-dots .dot');
 var side_menu = document.getElementById("menu_side");
 var btn_open = document.getElementById("btn_open");
 var body = document.getElementById("body");
 
+function showSlides() {
+    slides.forEach((slide, index) => {
+        slide.style.display = "none";
+        dots[index].classList.remove("active");
+    });
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 4000); // Cambia cada 4 segundos
+}
+
+function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
+}
+
+showSlides();
 //Evento para mostrar y ocultar menú
     function open_close_menu(){
         body.classList.toggle("body_move");
